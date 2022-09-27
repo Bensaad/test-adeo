c45a70ea6e1a62d0cd6ede64afd4059e67c0b956
sgamidb <36730095+sgamidb@users.noreply.github.com>
2022-09-27 14:55:32 +0200

    First commit

---
+++ test/filter.test.js
@@ -0,0 +1,9 @@
+import { filter } from "../app.js";
+
+describe('filter test', () => {
+    it('should show the animals matching with the ry string pattern', () => {
+        const result = filter('ry');
+
+        expect(result).toEqual('[{"name":"Uzuzozne","people":[{"name":"Lillie Abbott","animals":[{"name":"John Dory"}]}]},{"name":"Satanwi","people":[{"name":"Anthony Bruno","animals":[{"name":"Oryx"}]}]}]');
+    });
+});
